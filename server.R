3c0c31bbb951a1bc330f05eb27a4bda3cdc9e401
Tim Sloan <drtimsloan@gmail.com>
2016-04-01 11:35:04 +0100

    Improved algorithm to use all data robustly

--- server.R
+++ server.R
@@ -17,8 +17,8 @@ options(stringsAsFactors = FALSE)
 unig <- readLines("data/unigrams.txt")
 stopg <- readLines("data/stopgrams.txt")
 big <- fread("data/bigrams.txt")
-trig <- fread("data/trig.txt", nrow=1500000)
-quadg <- fread("data/quadg.txt", nrow=1500000)
+trig <- fread("data/trig.txt")
+quadg <- fread("data/quadg.txt")
 fiveg <- fread("data/fiveg.txt")
 sixg <- fread("data/sixg.txt")
 
@@ -27,7 +27,6 @@ setkey(trig,ngram)
 setkey(quadg,ngram)
 setkey(fiveg,ngram)
 setkey(sixg,ngram)
-
 
 shinyServer(function(input, output) {
 
@@ -46,11 +45,18 @@ shinyServer(function(input, output) {
         
         final <- in_clean[length(in_clean)]
         
-        if(!final %in% unig){in_clean[length(in_clean)] <- unig[agrepl(final, unig,max.distance=0.2)][1]}
+        if(!final %in% unig){
+            correct <- unig[agrepl(final, unig,max.distance=0.2)][1]
+            if(is.na(correct)){correct <- unig[agrepl(final, unig,max.distance=0.5)][1]}
+            in_temp <- unlist(strsplit(in_raw, " "))
+            in_clean[length(in_clean)] <- correct
+            ifelse(length(in_temp)==1,in_raw <- correct,{
+                in_raw <- paste(c(in_temp[1:(length(in_temp)-1)],correct),collapse=" ")
+            })
+        }
         
         in_text <- in_clean[in_clean %in% unig]
         in_size <- length(in_text)
-        in_mean <- in_text[!in_text %in% stopg]
         
         in_uni <- rev(in_text)[1]
         in_big <- paste(rev(in_text)[2:1],collapse=" ")
@@ -60,19 +66,25 @@ shinyServer(function(input, output) {
         
         out_big <- big[in_uni]
         out_trig <- trig[in_big,nomatch=0]
+        out_trig <- trig[in_big,nomatch=0]
         out_quad <- quadg[in_trig,nomatch=0]
         out_five <- fiveg[in_quad,nomatch=0]
         out_six <- sixg[in_five,nomatch=0]
         
-        predictions <- rbind(out_six, out_five, out_quad, out_trig, out_big)
-        predictions <- predictions[!predictions$pred %in% c(stopg,"who"),]
+        predictions <- rbind(out_six[!pred %in% stopg], out_six[pred %in% stopg],
+                             out_five[!pred %in% stopg], out_five[pred %in% stopg],
+                             out_quad[!pred %in% stopg], out_quad[pred %in% stopg],
+                             out_trig[!pred %in% stopg], out_trig[pred %in% stopg],
+                             out_big[!pred %in% stopg], out_big[pred %in% stopg])
+        
         answer <- predictions[1,pred]
         
-        if(is.null(answer)){answer<-out_big[1,pred]}
+        if(is.null(answer)){answer<-"the"}
+        if(is.na(answer)){answer<-"the"}
         
         output <- paste(in_raw, answer, sep=" ")
         output
-        
+
     })
 
 })
